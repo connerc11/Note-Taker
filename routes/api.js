bda3dda666ddc142b80f84d78a5955509da067c1
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-08-27 10:26:56 -0400

    updated to get the paths to flow

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+// const db = require('../db/db.json');
+
+const fs = require('fs');
+
+const util = require('util');
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+router.get("/api/notes", async (req, res) => {
+    const notes = await readFile("db/db.json", "utf8");
+    const notesArray = JSON.parse(notes) || []
+    res.json(notesArray);
+})
+router.post("/api/notes", async (req, res) => {
+    const notes = await readFile("db/db.json", "utf8");
+    const notesArray = JSON.parse(notes) || [];
+    console.log(req.body)
+    const newNotes = {title:req.body.title, text:req.body.text, id:2}
+    const newNotesArray = notesArray.concat(newNotes)
+    const finalNotes = writeFile("db/db.json", JSON.stringify(newNotesArray))
+    res.json(finalNotes)
+}) 
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-const path = require('path');
-const fs = require('fs');
 const express = require('express');
+const HTMLroutes = require('./routes/html.js');
+const apiRoutes = require('./routes/api.js')
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(HTMLroutes);
+app.use(apiRoutes);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
+
