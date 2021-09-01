bac3b8b5b7144a6d489795d73c175c215eb18815
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-01 16:43:33 -0400

    working on updating the delete function

--- routes/html.js
+++ routes/html.js
@@ -7,6 +7,16 @@ router.get("/", (req, res) => {
 
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
+
+// router.get("/api/notes/:id", (req, res) => {
+//     res.json(notes[req.params.id])
+// });
+router.delete("/api/notes/:id", (req, res) => {
+    const params = notes.splice(req.params.id, 1);
+    updateDb();
+    console.log("Deleted" + req.params.id);
 })
 
 module.exports = router;
