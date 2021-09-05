0be50e592b8fe9a9bd5c461bde217cb31d4e7797
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-05 11:41:49 -0400

    updated coding and started working on the delete route

--- routes/html.js
+++ routes/html.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { networkInterfaces } = require('os');
 const path = require('path');
 
 router.get("/", (req, res) => {
@@ -10,13 +11,16 @@ router.get("/notes", (req, res) => {
 });
 
 
-// router.get("/api/notes/:id", (req, res) => {
-//     res.json(notes[req.params.id])
-// });
+
 router.delete("/api/notes/:id", (req, res) => {
-    const params = notes.splice(req.params.id, 1);
-    updateDb();
-    console.log("Deleted" + req.params.id);
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+
+for (let i = 0; i < newNotes.length; i++)
+    console.log("Delete" + req.params.id);
+    if(newNotes[i].id == req.params.id){
+        newNotes.splice(i, 1);
+        break
+    }
 })
 
 module.exports = router;
